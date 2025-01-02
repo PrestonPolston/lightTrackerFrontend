8b68fbed894b1da5e1fffc21320f82e4d7ef123c
preston <polston13@outlook.com>
2025-01-02 18:23:36 -0500

    added a login function to handle submit a form with email and password curently logging the input to the console

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import GetUsers from "./components/GetUsers";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<GetUsers />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/getUsers" element={<GetUsers />} />
       </Routes>
     </>
   );
--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -1,2 +1,42 @@
-const Login = () => {};
+import { useState } from "react";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //test to see if form will log info to console
+    console.log("Email: ", email);
+    console.log("Password: ", password);
+  };
+
+  return (
+    <>
+      <form className="loginForm">
+        <h1>Login</h1>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" onClick={handleSubmit} value={"Login"} />
+        </div>
+      </form>
+    </>
+  );
+};
 export default Login;
